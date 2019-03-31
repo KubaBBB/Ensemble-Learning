99d27cbdb2e893b8b8d786ca7705a034aa86dc12
KubaBBB <jakubboliglowa1996@gmail.com>
2019-03-31 19:02:02 +0200

    Inital Commit - prototype, 2 types of classifiers, simple communication between agents

---
+++ DecisionTreeRegressorAgent.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+
+class DecisionTreeRegressorAgent:
+    def __init__(self, df, agent):
+        self._df = df;
+        self._tree = DecisionTreeRegressor();
+        self._agent = agent;
+
+    def split_dataframe(self):
+        train_set, test_set = train_test_split(self._df, test_size=0.2, random_state=42)
+        self._train_set = train_set
+        self._test_set = test_set
+        self._X_train = train_set.drop(['id', 'date', 'price'], axis=1)
+        self._y_train = train_set[['price']]
+
+        self._X_test = test_set.drop(['id', 'date', 'price'], axis=1)
+        self._y_test = test_set[['price']]
+
+    def calculate(self):
+        self._tree.fit(self._X_train, self._y_train)
+        y_predicted = self._tree.predict(self._X_test)
+        mse = mean_squared_error(self._y_test, y_predicted)
+        self.send_info(f'Mean Square error: {np.sqrt(mse)}')
+
+    def send_info(self, msg):
+        self._agent.send('main', msg)
---
+++ main.py
@@ -0,0 +1,37 @@
+from osbrain import run_agent
+from osbrain import run_nameserver
+import dataInfo
+import pandas as pd
+from DecisionTreeRegressorAgent import DecisionTreeRegressorAgent
+from LinearRegressionAgent import LinearRegressionAgent
+
+def log_message(agent, message):
+    agent.log_info('Received: %s' % message)
+
+if __name__ == '__main__':
+    # Dataset
+    df = pd.read_csv('./housesalesprediction/kc_house_data.csv');
+    dataInfo.print_corelation_heatmap(df)
+
+    # System deployment
+    ns = run_nameserver()
+    lrAgent = run_agent('lrAgent')
+    dtrAgent = run_agent('dtrAgent')
+
+    # System configuration
+    addr = dtrAgent.bind('PUSH', alias='main')
+    dtrAgent.connect(addr, handler=log_message)
+
+    addr2 = lrAgent.bind('PUSH', alias='main')
+    lrAgent.connect(addr2, handler=log_message)
+
+    #Encaplsulate agents
+    dtr = DecisionTreeRegressorAgent(df, agent=dtrAgent);
+    dtr.split_dataframe()
+    dtr.calculate()
+
+    lr = LinearRegressionAgent(df, agent=lrAgent);
+    lr.split_dataframe()
+    lr.calculate()
+
+    ns.shutdown()
